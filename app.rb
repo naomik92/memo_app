4e231a5cd98313286c17968476b3638be9c00ce5
naomik92 <naomik092@gmail.com>
2025-01-10 11:07:05 +0900

    JSONのデータをViewのerbファイルに取り込む

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 
 get '/index' do
+  @memos = memos
   erb :index
 end
 
@@ -9,10 +11,21 @@ get '/new' do
   erb :new
 end
 
-get '/memo' do
+get '/memo/:id' do
+  @id = params[:id]
+  @title = memos[params[:id].to_i - 1][:title]
+  @detail = memos[params[:id].to_i - 1][:detail]
   erb :show
 end
 
-get '/edit' do
+get '/memo/:id/edit' do
+  @title = memos[params[:id].to_i - 1][:title]
+  @detail = memos[params[:id].to_i - 1][:detail]  
   erb :edit
+end
+
+def memos
+  File.open('memo_data.json') do |file|
+    JSON.parse(file.read, symbolize_names: true)
+  end
 end
