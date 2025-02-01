0cf75fe2c3be0d179f83bd9f9e0a5de1b299b771
naomik92 <161175746+naomik92@users.noreply.github.com>
2025-02-02 07:27:02 +0900

    DBのデータを取得、変更、追加、削除を実装

--- Gemfile
+++ Gemfile
@@ -10,3 +10,4 @@ group :development do
 end
 gem 'erb_lint', require: false
 gem 'sinatra-contrib'
+gem 'pg'
--- app.rb
+++ app.rb
@@ -4,6 +4,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'json'
 require 'securerandom'
+require 'pg'
 
 get '/memos' do
   @memos = find_memos
@@ -50,33 +51,27 @@ patch '/memos/:id' do
   end
   memos[idx][:title] = params[:title]
   memos[idx][:detail] = params[:detail]
-  File.open('memo_data.json', 'w') do |file|
-    JSON.dump(memos, file)
-  end
+  results = connection.exec_params( "UPDATE memo_data SET title = $2, detail = $3 WHERE id = $1", [ params[:id], params[:title], params[:detail] ] )
   redirect '/memos'
 end
 
 delete '/memos/:id' do
-  memos = find_memos
-  memos.delete_if { |hash| hash[:id] == params[:id] }
-  File.open('memo_data.json', 'w') do |file|
-    JSON.dump(memos, file)
-  end
+  results = connection.exec_params( "DELETE FROM memo_data WHERE id = $1", [ params[:id] ] )
   redirect '/memos'
 end
 
+def connection
+  connection ||= PG::Connection.new( dbname: 'memo_app' )
+end
+
 def find_memos
-  File.open('memo_data.json') do |file|
-    JSON.parse(file.read, symbolize_names: true)
-  end
+  results = connection.exec( "SELECT * FROM memo_data" )
+  results.map { |result| result.transform_keys(&:to_sym) }
 end
 
 def add_memo(title, detail)
-  memos = find_memos
-  memos << { id: SecureRandom.uuid, title: title, detail: detail }
-  File.open('memo_data.json', 'w') do |file|
-    JSON.dump(memos, file)
-  end
+  id = SecureRandom.uuid
+  results = connection.exec_params( "INSERT INTO memo_data (id, title, detail) values ($1, $2, $3)", [ id, title, detail ] )
 end
 
 helpers do
